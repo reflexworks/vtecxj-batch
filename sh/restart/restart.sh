bfa880be013e96a89f246fa6e94e178b50c94434
noriko-terada <terada@virtual-tech.net>
2025-09-10 15:01:58 +0900

    Dockerイメージ一覧取得の前に認証処理を行うよう修正

--- sh/restart/restart.sh
+++ sh/restart/restart.sh
@@ -13,27 +13,25 @@ function restart() {
   REV=$1
   echo '[restart] REV='$REV
 
-  # deployment.yaml にタグを設定
   #echo '** deployment.yaml にタグを設定'
   sed -i s/XXX/$REV/ vtecx-deployment.yaml
 
-  # gcloud認証
-  gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file $CLASSES_DIR/$SERVICE_ACCOUNT_JSON --project $GCP_PROJECT_ID --quiet
-
-  # kubectlが使用できるようにする。
-  ./setup-config-auth.sh
-
-  # rollout restart
-  #echo '** restart'
+  #echo '** rollout restart'
   kubectl rollout restart deployments/$DEPLOYMENT_VTECX
 
-  # deploymentを元に戻す
+  #echo '** deployment.yaml のタグを元に戻す'
   sed -i s/$REV/XXX/ vtecx-deployment.yaml
 }
 
 cnt=0
 
-# Dockerイメージ一覧を取得
+#echo '** gcloud認証'
+gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file $CLASSES_DIR/$SERVICE_ACCOUNT_JSON --project $GCP_PROJECT_ID --quiet
+
+#echo '** kubectlが使用できるようにする。'
+./setup-config-auth.sh
+
+#echo '** Dockerイメージ一覧を取得'
 gcloud container images list-tags asia-northeast1-docker.pkg.dev/$GCP_PROJECT_ID/$ARTIFACTS_REPOSITORY/$GIT_PROJECT-$GIT_BRANCH | while read digest tags timestamp
 do
   cnt=$((cnt+1))
